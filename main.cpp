4a2277391895c4e5cc4240488da3fadf8293044e
dzisinho <dzisbogdan26@gmail.com>
2024-11-20 20:19:51 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int num, sum = 0;
+    cout << "Enter numbers (0): " << endl;
+    while (true) {
+        cin >> num;       
+        if (num == 0)    
+            break;
+        sum += num;       
+    }
+    cout << "sum = " << sum << endl;
+
+    return 0;
 }
