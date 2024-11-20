caec4ad0a20c6a7887e4a3d398ef65547f1481ca
dzisinho <dzisbogdan26@gmail.com>
2024-11-20 20:12:44 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int a, b;
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    int sum = (b - a + 1) * (a + b) / 2;
+    cout << sum << endl;
+
+    return 0;
 }
