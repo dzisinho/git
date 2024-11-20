9ba608b7ae37b8fd495987b87120c11ea808b864
dzisinho <dzisbogdan26@gmail.com>
2024-11-20 20:04:18 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    cout << "All numbers: ";
+    for (int i = a; i <= b; ++i) std::cout << i << " ";
+    cout << "\n";
+    cout << "Even numbers: ";
+    for (int i = a; i <= b; ++i) if (i % 2 == 0) std::cout << i << " ";
+    cout << "\n";
+    cout << "Odd numbers: ";
+    for (int i = a; i <= b; ++i) if (i % 2 != 0) std::cout << i << " ";
+    cout << "\n";
+    cout << "Numbers divisible by 7: ";
+    for (int i = a; i <= b; ++i) if (i % 7 == 0) std::cout << i << " ";
+    cout << "\n";
+
+    return 0;
 }
