ed483e01daca15bc485b9aba73a5fbf15fac922c
dzisinho <dzisbogdan26@gmail.com>
2024-11-20 20:44:35 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    int x, y;
+    int result = 1;
+    cin >> x >> y;
+    for (int i = 1; i <= y; ++i) {
+        result *= x;
+    }
+    cout << result << endl;
+
+    return 0;
 }
